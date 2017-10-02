78ccf25748cea061831bc36e3abc5ddb5024792d
Gaetz <gaetz@rufflerim.com>
2017-10-02 09:40:05 +0200

    Base

---
+++ code/step00_base/main.js
@@ -0,0 +1,42 @@
+let canvas;
+let canvasContext;
+let text;
+let keySpace;
+
+window.onload = function () {
+    load();
+
+    setInterval(() => {
+        update();
+        draw();
+    }, 1000 / 60);
+}
+
+function load() {
+    canvas = document.getElementById('gameCanvas');
+    canvasContext = canvas.getContext('2d');
+    document.addEventListener('keydown', detectKey, false);
+
+    text = 'Hello, Gaëtan';
+}
+
+function detectKey(e) {
+    if (e.keyCode == 32) {
+        keySpace = true;
+    }
+}
+
+function update() {
+    if (keySpace) {
+        text = 'Bien ou bien ?';
+    }
+}
+
+function draw() {
+    // Fill in black
+    canvasContext.fillStyle = 'black';
+    canvasContext.fillRect(0, 0, 800, 600);
+    // Draw in white
+    canvasContext.fillStyle = 'white';
+    canvasContext.fillText(text, 50, 50);
+}
